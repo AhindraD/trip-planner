9077b252cbdbf5e3cd559b151c16886164271316
Ahindra D <official.ahindra@gmail.com>
2022-08-05 15:12:11 +0530

    twailand css

--- src/App.js
+++ src/App.js
@@ -10,41 +10,75 @@ import Login from './components/Login';
 import Planning from './components/Planning';
 import Trips from './components/Trips';
 import Modify from './components/Modify'
+import { useState } from 'react';
+
+// import { collection, getDocs, addDoc } from "firebase/firestore";
+import { db, auth } from './firebase-config';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 function App() {
   let trips = useSelector(state => state.trips);
   const dispatchREDUX = useDispatch();
 
+  let [user, setUser] = useState(null);
+  let [loginMsg, setLoginMsg] = useState('Login');
+
+  function logInEmail() {
+    if (loginMsg === 'Login') {
+      createUserWithEmailAndPassword(auth, )
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          // ...
+        })
+        .catch((error) => {
+          const errorMessage = error.message;
+          alert(errorMessage);
+        });
+    }
+    else {
+      signOut(auth).then(() => {
+        // Sign-out successful.
+      }).catch((error) => {
+        // An error happened.
+        const errorMessage = error.message;
+        alert(errorMessage);
+      });
+      navigate('/');
+    }
+  }
+
   let navigate = useNavigate();
+
+
+
+
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
       {/* <div className="head">
-          <div className="title1" onClick={() => { navigate('/') }}
-          >Trip Planner</div>
+        <div className="title1" onClick={() => { navigate('/') }}
+        >Trip Planner</div>
 
-          {user !== null ?
-            <div className='user-preview'>
-              <img src={user.userPicture} alt="" className="user-img" />
-              <p className="user-name">{user.userName}</p>
-            </div> : null}
+        {user !== null ?
+          <div className='user-preview'>
+            <img src={user.userPicture} alt="" className="user-img" />
+            <p className="user-name">{user.userName}</p>
+          </div> : null}
 
-          <button className="login1" onClick={() => {
-            logInGoogle();
-          }}>{loginMsg}</button>
-        </div>
-*/}
-      <div className="mid">
+        <button className="login1" onClick={() => {
+          logInEmail();
+        }}>{loginMsg}</button>
+      </div> */}
+
+      {/* <div className="mid"> */}
         <Routes>
-          <Route path='/' element={<Login />} />
+          <Route path='/' element={<Login logInEmail={logInEmail} />} />
           <Route path='/profile' element={<Profile />} />
           <Route path='/planning' element={<Planning />} />
           <Route path='/trips' element={<Trips />} />
           <Route path='/trips/tripID' element={<Modify />} />
         </Routes>
-      </div>
+      {/* </div> */}
     </div>
   );
 }
